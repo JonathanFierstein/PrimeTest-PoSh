d238e6802c035f01a9b119da4d93740030265bb3
Jonathan Fierstein <jonathan.fierstein@gmail.com>
2024-05-04 22:11:00 -0700

    PrimeTest-PoSh Repository Initial Commit
    	This is the initial commit for the repository.   The files included are just
    	basic repo files, the license, .gitignore, and README.md.   In addition,
    	an essentially empty PrimeTest.ps1 which will be where the Powershell script
    	is located one the coding for it is started.
    
    	This repository is meant to be a simple coding excercise for students learning
    	Powershell.  It will involve coding a powershell script which contains a function
    	that will evaluate a range of numebrs for the presence of prime numbers and return
    	any prime numbers identified within the range.

---
+++ PrimeTest.ps1
@@ -0,0 +1,16 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2022 v5.8.213
+	 Created on:   	5/4/2024 9:53 PM
+	 Created by:   	Jonathan Fierstein
+	 Organization: 	
+	 Filename: PrimeTest.ps1 	
+	===========================================================================
+	.DESCRIPTION
+		This script contains a function, Get-PrimeNumber, which will evaluate a range of numebrs
+		for the presence of prime numbers.   
+#>
+
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# PrimeTest-Posh  
+
+This is a powershell based version of my simple project for students to 
+develop a Powershell script that contains a function which will evaluate a 
+range of numbers for the presence of prime numbers.   
